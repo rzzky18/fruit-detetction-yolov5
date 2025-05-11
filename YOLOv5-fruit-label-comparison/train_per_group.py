cb9633fdec2cb4146d47b78724691986cba70425
rzzky18 <m.rizkypurnama56@gmail.com>
2025-05-11 23:13:15 +0700

    Update train_per_group.py

--- YOLOv5-fruit-label-comparison/train_per_group.py
+++ YOLOv5-fruit-label-comparison/train_per_group.py
@@ -8,9 +8,9 @@ project_path = "runs/train"  # direktori output hasil training (di luar yolov5)
 # Training per grup
 subprocess.run([
     "python", os.path.join(yolov5_path, "train.py"),
-    "--img", "416",                        # resolusi gambar diperkecil agar cepat
-    "--batch", "8",                        # batch kecil agar ringan
-    "--epochs", "5",                       # hanya 5 epoch untuk uji coba cepat
+    "--img", "640",                        # resolusi gambar 
+    "--batch", "16",                        # batch 
+    "--epochs", "50",                      
     "--data", "train_per_group.yaml",     # file konfigurasi dataset
     "--weights", "yolov5s.pt",            # model awal
     "--name", "fruits_per_group",         # nama folder hasil
